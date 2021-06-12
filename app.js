101c983b4297857352cd30bacf13e40930d80e85
kimkevin90 <65535673+kimkevin90@users.noreply.github.com>
2021-06-12 13:01:08 +0900

    change

--- app.js
+++ app.js
@@ -5,5 +5,9 @@ app.get("/", function (req, res) {
   res.send("Hello elastic");
 });
 
+app.get("/new", function (req, res) {
+  res.send("new hello");
+});
+
 app.listen(process.env.PORT || 5000);
 module.exports = app;
